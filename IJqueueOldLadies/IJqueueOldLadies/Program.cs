fa0449659f57323c639f0ed8011a37bdc0c8cbca
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-15 16:41:51 +0500

    huursLine

--- IJqueueOldLadies/IJqueueOldLadies/Program.cs
+++ IJqueueOldLadies/IJqueueOldLadies/Program.cs
@@ -9,17 +9,14 @@ namespace IJqueueOldLadies
             int receptionTime = 10;
             int minutesHour = 60;
             int countPeople;
-            int hours;
-            int minutes;
+            int hoursLine;
+            int minutesLine;
             Console.WriteLine("Введите количество человек в очереди");
             countPeople = Convert.ToInt32(Console.ReadLine());
-            countPeople *= receptionTime;
-            minutes = countPeople;
-            countPeople /= minutesHour;
-            hours = countPeople;
-            countPeople *= minutesHour;
-            minutes -= countPeople;
-            Console.WriteLine($"Вы должны отстоять в очереди {hours} часа : {minutes} минут");
+            receptionTime *= countPeople;
+            hoursLine = receptionTime / minutesHour;
+            minutesLine = receptionTime % minutesHour;
+            Console.WriteLine($"Вы должны отстоять в очереди {hoursLine} часа : {minutesLine} минут");
         }
     }
 }
