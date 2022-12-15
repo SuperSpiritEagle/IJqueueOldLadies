988c93aba39c83139d98b8c49ba74c7d02347292
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-15 14:47:29 +0500

    init

---
+++ IJqueueOldLadies/IJqueueOldLadies/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IJqueueOldLadies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int receptionTime = 10;
+            int countPeople;
+            Console.WriteLine("Введите количество человек в очереди");
+            countPeople = Convert.ToInt32(Console.ReadLine());
+            countPeople *= receptionTime;
+            var queueTime = TimeSpan.FromMinutes(countPeople);
+            Console.WriteLine($"Вы должны отстоять в очереди {queueTime}");
+        }
+    }
+}
